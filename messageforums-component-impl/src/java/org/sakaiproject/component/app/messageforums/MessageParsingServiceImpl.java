341886b3bb13138370a1128e93be62028cef2748
Marc Savitsky <marc.savitsky@it.ox.ac.uk>
2012-10-08 13:08:22 +0100

    WL-2398 Add html escape and Adams testcase

--- messageforums-component-impl/src/java/org/sakaiproject/component/app/messageforums/MessageParsingServiceImpl.java
+++ messageforums-component-impl/src/java/org/sakaiproject/component/app/messageforums/MessageParsingServiceImpl.java
@@ -7,8 +7,14 @@ public class MessageParsingServiceImpl implements MessageParsingService {
 
 	public String parse(String message) {
 		String withMarkup = message.replaceAll("\\r", "");
+		
+		withMarkup = withMarkup.replaceAll("&", "&amp;");
+		withMarkup = withMarkup.replaceAll("<", "&lt;");
+		withMarkup = withMarkup.replaceAll(">", "&gt;");
+		withMarkup = withMarkup.replaceAll("\"", "&quot;");
+		
 		withMarkup = withMarkup.replaceAll("\\n", "<br />");
-		withMarkup = withMarkup.replaceAll( "</?a[^>]*>", "" );
+		//withMarkup = withMarkup.replaceAll( "</?a[^>]*>", "" );
 		withMarkup = withMarkup.replaceAll("(@)?((https?|ftps?)://)?[a-zA-Z_0-9\\-]+(\\.\\w[a-zA-Z_0-9\\-]+)+(/[#&\\n\\-=?\\+\\%/\\.\\w]+)?", "<a href='$0'>$0</a>");
 		return withMarkup;
 	}
--- messageforums-component-impl/src/test/org/sakaiproject/component/app/messageforums/TestMessageParsingServiceImpl.java
+++ messageforums-component-impl/src/test/org/sakaiproject/component/app/messageforums/TestMessageParsingServiceImpl.java
@@ -10,15 +10,20 @@ public class TestMessageParsingServiceImpl extends TestCase {
 
 	private String message1="The quick brown fox jumped over the lazy dog\nwww.bbc.co.uk";
 	private String result1="The quick brown fox jumped over the lazy dog\nwww.bbc.co.uk";
+	private String storage1="The quick brown fox jumped over the lazy dog<br /><a href='www.bbc.co.uk'>www.bbc.co.uk</a>";
 	
 	private String message2="The quick brown fox jumped over the lazy dog<br />www.bbc.co.uk";
-	private String result2="The quick brown fox jumped over the lazy dog\nwww.bbc.co.uk";
+	private String result2="The quick brown fox jumped over the lazy dog<br />www.bbc.co.uk";
+	private String storage2="The quick brown fox jumped over the lazy dog&lt;br /&gt;<a href='www.bbc.co.uk'>www.bbc.co.uk</a>";
 	
-	private String message3="The quick brown fox jumped over the lazy dog\n<a href='www.bbc.co.uk'>www.bbc.co.uk</a>";
-	private String result3="The quick brown fox jumped over the lazy dog\nwww.bbc.co.uk";
-	
-	private String storage="The quick brown fox jumped over the lazy dog<br /><a href='www.bbc.co.uk'>www.bbc.co.uk</a>";
-	
+	private String message3="just sending http://news.bbc.co.uk and expecting WebLearn to mark it\nup?";
+	private String result3="just sending http://news.bbc.co.uk and expecting WebLearn to mark it\nup?";
+	private String storage3="just sending <a href='http://news.bbc.co.uk'>http://news.bbc.co.uk</a> and expecting WebLearn to mark it<br />up?";
+				
+	private String message4="<b>Fred bloggs</b> visits the bbc website: http://bbc.co.uk.\n\n<i>Alice</i> on the other hand doesnt.\n\"";
+	private String result4="<b>Fred bloggs</b> visits the bbc website: http://bbc.co.uk.\n\n<i>Alice</i> on the other hand doesnt.\n\"";
+	private String storage4="&lt;b&gt;Fred bloggs&lt;/b&gt; visits the bbc website: <a href='http://bbc.co.uk'>http://bbc.co.uk</a>.<br /><br />&lt;i&gt;Alice&lt;/i&gt; on the other hand doesnt.<br />&quot;";
+			
 	public TestMessageParsingServiceImpl(String name) {
 		super(name);
 	}
@@ -27,7 +32,7 @@ public class TestMessageParsingServiceImpl extends TestCase {
 	
 	@Test
 	public void test1() {
-		assertEquals(storage, 
+		assertEquals(storage1, 
 				messageParsingService.parse(message1));
 		assertEquals(result1, 
 				messageParsingService.format(messageParsingService.parse(message1)));
@@ -35,7 +40,7 @@ public class TestMessageParsingServiceImpl extends TestCase {
 	
 	@Test
 	public void test2() {
-		assertEquals(storage, 
+		assertEquals(storage2, 
 				messageParsingService.parse(message2));
 		assertEquals(result2, 
 				messageParsingService.format(messageParsingService.parse(message2)));
@@ -43,10 +48,18 @@ public class TestMessageParsingServiceImpl extends TestCase {
 	
 	@Test
 	public void test3() {
-		assertEquals(storage, 
+		assertEquals(storage3, 
 				messageParsingService.parse(message3));
 		assertEquals(result3, 
 				messageParsingService.format(messageParsingService.parse(message3)));
+	}
+	
+	@Test
+	public void test4() {
+		assertEquals(storage4, 
+				messageParsingService.parse(message4));
+		assertEquals(result4, 
+				messageParsingService.format(messageParsingService.parse(message4)));
 	}
 	
 	public static void main(String[] args) {
